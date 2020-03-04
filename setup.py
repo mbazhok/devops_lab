1ae6d91f8dae1dc5d20fe71cfcffd6ec18489ffc
Maksim Bazhok <60423839+mbazhok@users.noreply.github.com>
2020-03-04 21:03:59 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pr-stats",
+    packages=find_packages(),
+    scripts=["pr-stats"],
+    version="0.1",
+    author="Maksim Bazhok",
+    author_email="custom@gmail.com",
+    description="Homework4 app.",
+    license="MIT"
+)
